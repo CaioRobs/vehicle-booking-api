85ab8d686f7292b9b75337bed0232c408017716a
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-23 16:28:18 -0300

    add env file to app container

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { env } from 'process';
 
+const mongoURI = env.MONGO_URI;
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot(mongoURI)],
   controllers: [AppController],
   providers: [AppService],
 })
