7ca536a45329534793e96bcd1be92a13ef428682
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-24 03:05:27 -0300

    remove unused variables

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { CreateVehicleDto } from './dto/create-vehicle.dto';
 
