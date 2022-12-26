fb516443c7a47ead1f0a88a4842f97c310ed12ef
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-26 16:08:09 -0300

    fix lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
+import { env } from 'process';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { env } from 'process';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 const mongoURI = env.MONGO_URI;
 @Module({
-  imports: [MongooseModule.forRoot(mongoURI), VehiclesModule, UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot(mongoURI),
+    VehiclesModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
