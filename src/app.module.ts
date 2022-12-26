70ccc3847216b032fd7c917d45751e09d2ab9571
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-26 09:33:09 -0300

    initiate auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { env } from 'process';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 const mongoURI = env.MONGO_URI;
 @Module({
-  imports: [MongooseModule.forRoot(mongoURI), VehiclesModule, UsersModule],
+  imports: [MongooseModule.forRoot(mongoURI), VehiclesModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
