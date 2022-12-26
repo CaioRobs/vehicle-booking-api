ce3f811b5516bbeb9fa2b94fb52712b2c361ea06
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-26 16:09:52 -0300

    adds jwt sign

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: any) {
-    console.log({ controller: { req, user: req.user } });
-    delete req.user.password;
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,20 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { UserWithoutPassword } from 'src/users/schemas/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import { User, UserWithoutPassword } from 'src/users/schemas/user.schema';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(
     email: string,
     password: string,
   ): Promise<UserWithoutPassword | null> {
     const user = await this.usersService.findByEmailWithPassword(email);
-    console.log({ user });
     if (user && user.password === password) {
       return this.usersService.findByEmail(email);
     }
     return null;
+  }
+
+  async login(user: User): Promise<{ access_token: string }> {
+    const payload = {
+      email: user.email,
+      username: user.username,
+      name: user.name,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
