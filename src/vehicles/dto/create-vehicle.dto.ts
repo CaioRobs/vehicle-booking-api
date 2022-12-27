0bbd5aa835332a2aec6e0c289c64014aefbf0634
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-27 02:28:53 -0300

    remove unecessary post endpoint

--- src/vehicles/dto/create-vehicle.dto.ts
+++
@@ -1,7 +0,0 @@
-export class CreateVehicleDto {
-  readonly brand: string;
-  readonly model: string;
-  readonly year: string;
-  readonly color: string;
-  readonly available: boolean;
-}
--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -13,7 +13,6 @@ import {
   Req,
 } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
-import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { Vehicle } from './schemas/vehicle.schema';
 import { Response } from 'express';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
@@ -22,11 +21,6 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 export class VehiclesController {
   constructor(private readonly vehiclesService: VehiclesService) {}
-
-  @Post()
-  create(@Body() createVehicleDto: CreateVehicleDto) {
-    return this.vehiclesService.create(createVehicleDto);
-  }
 
   @Get()
   async findAll(@Res() res: Response) {
