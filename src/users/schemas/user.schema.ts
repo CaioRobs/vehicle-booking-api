dc96fd7d8f8d49e2f6670d68ff69c77b94cce28e
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-27 02:25:21 -0300

    initiatind documentation with swagger

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,23 +1,32 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
+@ApiTags('Users')
 export class User {
   @Prop({ required: true })
+  @ApiProperty()
   name: string;
 
   @Prop({ required: true, unique: true })
+  @ApiProperty()
   username: string;
 
   @Prop({ required: true, unique: true })
+  @ApiProperty()
   email: string;
 
   @Prop({ required: true, select: false })
+  @ApiProperty()
   password: string;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Owner' })
+  @ApiProperty({
+    description: 'Vehicle id in case the user currently has one',
+  })
   vehicle: mongoose.Types.ObjectId | null;
 }
 
--- src/vehicles/schemas/vehicle.schema.ts
+++ src/vehicles/schemas/vehicle.schema.ts
@@ -1,23 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { HydratedDocument } from 'mongoose';
 
 export type VehicleDocument = HydratedDocument<Vehicle>;
 
 @Schema({ timestamps: true })
+@ApiTags('Vehicles')
 export class Vehicle {
   @Prop({ required: true })
+  @ApiProperty()
   brand: string;
 
   @Prop({ required: true })
+  @ApiProperty()
   model: string;
 
   @Prop({ required: true })
+  @ApiProperty()
   color: string;
 
   @Prop()
+  @ApiProperty()
   year: string;
 
   @Prop({ required: true, default: true })
+  @ApiProperty({
+    description: 'whether the car is available or not',
+    default: false,
+  })
   available: boolean;
 }
 
