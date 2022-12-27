9eb08a7d7a8a48b0b58b679bcfee89bf11e149aa
Caio R. Vieira <caio.robs98@gmail.com>
2022-12-27 02:38:33 -0300

    better login error description

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -16,7 +16,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   ): Promise<UserWithoutPassword> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Wrong username or password');
     }
     return user;
   }
